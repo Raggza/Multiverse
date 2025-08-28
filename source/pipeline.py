685afd2bf1fb75acac42e4450f3d486b5d0ed8ab
namnx <ryomapro123@gmail.com>
2025-08-29 00:17:12 +0700

    update dryad and dwarf

--- source/pipeline.py
+++ source/pipeline.py
@@ -1,6 +1,9 @@
 from source.modules.angel import AngelImgGenerate
 from source.modules.demon import DemonImgGenerate
 from source.modules.dragon import DragonImgGenerate
+from source.modules.dryad import DryadImgGenerate
+from source.modules.dwarf import DwarfImgGenerate
+
 from source.schemas.input import CharacterInput
 
 
@@ -9,6 +12,8 @@ class Pipeline:
         self.angel_img_generate = AngelImgGenerate()
         self.demon_img_generate = DemonImgGenerate()
         self.dragon_img_generate = DragonImgGenerate()
+        self.dryad_img_generate = DryadImgGenerate()
+        self.dwarf_img_generate = DwarfImgGenerate()
 
     def invoke(self, character_input: CharacterInput) -> str:
         if character_input.race.lower() == "angel":
@@ -16,5 +21,9 @@ class Pipeline:
         if character_input.race.lower() == "demon":
             return self.demon_img_generate.overlay_images()
         if character_input.race.lower() == "dragon":
-            return self.dragon_img_generate.overlay_images()
+            return self.dragon_img_generate.overlay_images(character_input)
+        if character_input.race.lower() == "dryad":
+            return self.dryad_img_generate.overlay_images()
+        if character_input.race.lower() == "dryad":
+            return self.dwarf_img_generate.overlay_images(character_input)
         return ""
