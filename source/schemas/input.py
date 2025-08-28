7d5e72e03dd7b897fbba14f98a6de6f723907c3a
namnx <ryomapro123@gmail.com>
2025-08-29 00:18:15 +0700

    delete key model

--- source/schemas/input.py
+++ source/schemas/input.py
@@ -4,4 +4,3 @@ class CharacterInput(BaseModel):
     race: str = Field(..., title="Race of the character")
     sub_race: str = Field(..., title="Subrace or detail description of the character")
     weapon: str = Field(..., title="Weapon of the character")
-    model_name: str = Field(..., title="Model name")
