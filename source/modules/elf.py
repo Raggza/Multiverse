744afaeca7ac1915458fb676c116383811a8f199
namnx <ryomapro123@gmail.com>
2025-08-29 01:09:12 +0700

    add elf, giant, gnome, doblin

---
+++ source/modules/elf.py
@@ -0,0 +1,56 @@
+import base64
+import io
+
+from PIL import Image
+from pathlib import Path
+from collections import defaultdict
+
+from source.utils.config_utils import get_config
+
+
+class ElfImgGenerate:
+    def __init__(self):
+        self.root_file = get_config("image", "elf")
+        self.part = get_config("image", "elf_parts")
+
+    def _get_parts_list(self):
+        body_parts = defaultdict(list)
+        for f in self.part:
+            prefix = f.split("-")[0]
+            body_parts[prefix].append(f)
+
+        parts_choice_order = dict(
+            sorted(body_parts.items(), key=lambda item: int(item[0]))
+        )
+        temp_list = [
+            item for sublist in parts_choice_order.values() for item in sublist
+        ]
+        body_parts_list = []
+        for item in temp_list[::-1]:
+            path = Path(f"{self.root_file}\\{item}")
+            body_parts_list.append(path)
+
+        return body_parts_list
+
+    def overlay_images(self):
+        image_paths = self._get_parts_list()
+        base = Image.open(image_paths[0]).convert("RGBA")
+        canvas = Image.new("RGBA", base.size, (255, 255, 255, 0))
+
+        for path in image_paths:
+            img = Image.open(path).convert("RGBA")
+            canvas.paste(img, (0, 0), img)
+
+        buffer = io.BytesIO()
+        canvas.save(buffer, format="PNG")
+        base64_str = base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+        return base64_str
+
+
+if __name__ == "__main__":
+    generator = ElfImgGenerate()
+    img_base64 = generator.overlay_images()
+    img_bytes = base64.b64decode(img_base64)
+    img = Image.open(io.BytesIO(img_bytes))
+    img.show()
--- source/pipeline.py
+++ source/pipeline.py
@@ -3,6 +3,10 @@ from source.modules.demon import DemonImgGenerate
 from source.modules.dragon import DragonImgGenerate
 from source.modules.dryad import DryadImgGenerate
 from source.modules.dwarf import DwarfImgGenerate
+from source.modules.elf import ElfImgGenerate
+from source.modules.giant import GiantImgGenerate
+from source.modules.gnome import GnomeImgGenerate
+from source.modules.goblin import GoblinImgGenerate
 
 from source.schemas.input import CharacterInput
 
@@ -14,6 +18,10 @@ class Pipeline:
         self.dragon_img_generate = DragonImgGenerate()
         self.dryad_img_generate = DryadImgGenerate()
         self.dwarf_img_generate = DwarfImgGenerate()
+        self.elf_img_generate = ElfImgGenerate()
+        self.giant_img_generate = GiantImgGenerate()
+        self.gnome_img_generate = GnomeImgGenerate()
+        self.goblin_img_generate = GoblinImgGenerate()
 
     def invoke(self, character_input: CharacterInput) -> str:
         if character_input.race.lower() == "angel":
@@ -26,4 +34,12 @@ class Pipeline:
             return self.dryad_img_generate.overlay_images()
         if character_input.race.lower() == "dryad":
             return self.dwarf_img_generate.overlay_images(character_input)
+        if character_input.race.lower() == "elf":
+            return self.elf_img_generate.overlay_images()
+        if character_input.race.lower() == "giant":
+            return self.giant_img_generate.overlay_images()
+        if character_input.race.lower() == "gnome":
+            return self.gnome_img_generate.overlay_images()
+        if character_input.race.lower() == "goblin":
+            return self.goblin_img_generate.overlay_images()
         return ""
